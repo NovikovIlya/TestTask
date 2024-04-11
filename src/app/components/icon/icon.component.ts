418c44ac553c34c952650597f9e1fa8961ca106b
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-04-11 11:45:54 +0300

    Логика для последовательных щелчков

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,31 +1,50 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Component, OnInit } from '@angular/core';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import * as iconsSvg from "@fortawesome/free-solid-svg-icons";
-
+import { fromEvent, interval } from 'rxjs';
+import { buffer, debounceTime, filter } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-icon',
+  selector: "app-icon",
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule],
-  templateUrl: './icon.component.html',
-  styleUrl: './icon.component.css'
+  imports: [FontAwesomeModule],
+  templateUrl: "./icon.component.html",
+  styleUrl: "./icon.component.css",
 })
 
+export class IconComponent implements OnInit {
+  randomIndex = 0;
+  iconsSvg: any = iconsSvg;
+  names: string[] = [];
+  x = "fa0";
+  clickDisabled = false;
 
-export class IconComponent {
-  randomIndex = 0
-  iconsSvg : any = iconsSvg
+  ngOnInit(): void {
+    this.names = Object.keys(iconsSvg);
+    const buttonClick = fromEvent(document.querySelector(".btn")!, "click");
 
-  //массив имен
-  names = Object.keys(iconsSvg)
-  x = 'fa0'
+    // буфер щелчков если больше 1
+    buttonClick
+      .pipe(buffer(buttonClick
+        .pipe(debounceTime(3000))),
+        filter((arr) => arr.length > 1)
+      )
+      .subscribe(() => {
+        this.showRandomIcon();
+      });
+  }
 
-  // метод рандомной иконки
   showRandomIcon() {
+    if (this.clickDisabled) {
+      return;
+    }
+
+    this.clickDisabled = true;
+
     setTimeout(() => {
       this.randomIndex = Math.floor(Math.random() * this.names.length);
-      this.x = this.names[this.randomIndex]
+      this.x = this.names[this.randomIndex];
+      this.clickDisabled = false;
     }, 3000);
   }
 }
